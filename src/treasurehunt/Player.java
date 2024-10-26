b0701f176a0569c9fe5ef184e33f80ecef45bfb4
Nuhan Berke Hallaç <berkehallac@gmail.com>
2024-10-26 16:08:28 +0300

    Added player methods.

--- src/treasurehunt/Player.java
+++ src/treasurehunt/Player.java
@@ -6,22 +6,53 @@ package src.treasurehunt;
  */
 
 public class Player extends MapItem {
+    private int points;
+    private int lives;
 
     // Default constructor
     public Player() {
         super(0, 0, 'P');
+        this.points = 100;
+        this.lives = 2;
     }
 
     // Copy constructor
     public Player(Player player) {
         super(player);
+        this.points = player.points;
+        this.lives = player.lives;
     }
 
     // Parameterized constructor
     public Player(int x, int y) {
         super(x, y, 'P');
+        this.points = 100;
+        this.lives = 2;
     }
 
     // Getters and setters
 
+    public int getPoints() {
+        return points;
+    }
+
+    public void addPoints(int points) {
+        this.points += points;
+    }   
+
+    public void removePoints(int points) {
+        this.points -= points;
+    }
+
+    public int getLives() {
+        return lives;
+    }
+
+    public void addLives(int lives) {
+        this.lives += lives;
+    }
+
+    public void removeLives(int lives) {
+        this.lives -= lives;
+    }
 }
